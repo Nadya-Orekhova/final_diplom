339d91994dde1337e716e4d1f4b3f85c85c3bdd4
Nadya Orekhova <ninilavaet@gmail.com>
2024-10-07 13:38:19 +0300

    + celery.py

---
+++ orders/celery.py
@@ -0,0 +1,10 @@
+from celery import Celery
+
+from orders.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
+
+
+celery_app = Celery('orders', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
+
+
